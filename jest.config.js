bd40b854121baca647fa3f58207b4a65a486a33d
Wayne Graham <wgraham@clir.org>
2025-06-06 08:44:15 -0400

    Configure Jest with Babel

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 export default {
   testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.jsx'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|s[ac]ss)$': 'identity-obj-proxy',
+  },
 };
