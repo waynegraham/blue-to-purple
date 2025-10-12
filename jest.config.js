f50d85e88c4c89b5202348ed48998fa86d3f366c
Wayne Graham <wgraham@clir.org>
2025-10-12 13:33:42 -0400

    Document evaluation findings and align Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 export default {
   testEnvironment: 'jsdom',
   extensionsToTreatAsEsm: ['.jsx'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]sx',
+    '**/?(*.)+(spec|test).[jt]sx',
+  ],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
