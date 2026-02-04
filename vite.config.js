0f6d68ac599ce07f03bf1801e23f019d6efbcff4
Wayne Graham <wgraham@clir.org>
2026-02-04 09:48:43 -0500

    Fix the issue with 404 errors with direct link to test mode

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/blue-to-purple',
+  base: '/blue-to-purple/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
